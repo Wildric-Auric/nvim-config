3a1642f6e5c402089e04fce22db8b292dde484f8
Wildric-Auric <wildric.auric@gmail.com>
2025-03-14 08:59:55 +0100

    added better bindings for tab navigation

--- lua/scripts/remap.lua
+++ lua/scripts/remap.lua
@@ -11,9 +11,17 @@ vim.keymap.set("n", "<leader>l",  "<C-w>l")
 vim.keymap.set("n", "<leader>j",  "<C-w>j")
 vim.keymap.set("n", "<leader>w",  "<C-w>w")
 
+vim.keymap.set("n", "<leader>h", "<C-w>h");
+vim.keymap.set("n", "<leader>l", "<C-w>l");
+vim.keymap.set("n", "<leader>j", "<C-w>j");
+vim.keymap.set("n", "<leader>k", "<C-w>k");
+vim.keymap.set("n", "<leader>w", "<C-w>w");
+
 vim.keymap.set("x", "<leader>cc", ":s/^/\\/\\/<CR>") --Comment selected lines
 
 vim.keymap.set("n", "<left>",  "i")
 vim.keymap.set("n", "<right>", "a")
 vim.keymap.set("n", "<up>",    "i")
 vim.keymap.set("n", "<down>",  "a")
+
+vim.keymap.set("n", "<leader>tt",  ":NvimTreeToggle<CR>")

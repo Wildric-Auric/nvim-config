bcf7c739d80c706c9ffa1aa00af41c59e36bb364
Oussama Soudassi <wildric.auric@gmail.com>
2025-11-09 17:44:45 +0100

    added function to open hex files in read only

--- init.lua
+++ init.lua
@@ -17,3 +17,13 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+vim.api.nvim_create_autocmd("BufReadPost", {
+    pattern = {"*.bin", "*.exe", "*.dat", "*.o"},  -- binary file extensions
+    callback = function()
+        vim.cmd("%!xxd")             -- convert buffer to hex
+        vim.cmd("setlocal binary")   -- treat as binary
+        vim.cmd("setlocal readonly") -- make buffer read-only
+        vim.cmd("setlocal nowrap")   -- optional: no line wrapping
+    end
+})
+
